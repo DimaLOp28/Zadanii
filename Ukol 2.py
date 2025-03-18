ca705f69450518fa328def69a07c5a3b8283009c
DimaLOp28 <sofiia.nehreba@infis.cz>
2025-03-18 10:52:09 +0100

    Update Ukol 2.py

--- Ukol 2.py
+++ Ukol 2.py
@@ -2,12 +2,12 @@
 num1 = float(input("Zadajte 1 čislo: "))
 num2 = float(input("Zadajte 2 čislo: "))
  
-sum_result = num1 + num2
-diff_result = num1 - num2
-prod_result = num1 * num2
-quot_result = num1 / num2 
+soucet = num1 + num2
+rozdil = num1 - num2
+soucin = num1 * num2
+podil = num1 / num2 
 
-print(f"Součet: {sum_result:.2f}")
-print(f"Rozdil: {diff_result:.2f}")
-print(f"Součin: {prod_result:.2f}")
-print(f"Podil: {quot_result:.2f}")
+print(f"Součet: {soucet:.2f}")
+print(f"Rozdil: {rozdil:.2f}")
+print(f"Součin: {soucin:.2f}")
+print(f"Podil: {podil:.2f}")
